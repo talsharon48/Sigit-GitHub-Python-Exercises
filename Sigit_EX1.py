2332a3b89b7fd46fdd5d51ec141162efcc5eb9c2
talsharon48 <talsharon48@gmail.com>
2019-03-06 17:37:57 +0200

    Created Menu/Options Function

--- Sigit_EX1.py
+++ Sigit_EX1.py
@@ -1,6 +1,22 @@
 DICT_OF_SECRET_MONEY = {"1234": 2000, "2345": 3000, "3456": 4000, "4567": 5000, "5678": 6000, "6789": 7000}
 
 
+# function that simulates ATM
+def options(secret_code):
+    dict_of_operations = {"A": None,
+                          "B": None,
+                          "C": None,
+                          "D": None}
+    # checking the validate of the option
+    while True:
+        op = input(
+            "Please Enter One Of These Options:\nA: Print Balance\nB: Withdrawal\nC: Change Secret Code\nD: Exit\n")
+        while not 'A' <= op <= 'D':
+            print("Invalid Input")
+            op = input(
+                "Please Enter One Of These Options:\nA: Print Balance\nB: Withdrawal\nC: Change Secret Code\nD: Exit\n")
+
+
 # function that checks if the secret num is exists
 def check_secret_num():
     code = input("Please Enter Secret Code: ")
@@ -22,6 +38,7 @@ def main():
     correct_secret, code = check_secret_num()
     if correct_secret:
         print("Connected Successfully")
+        options(code)
     else:
         print("Bye Bye")
 
