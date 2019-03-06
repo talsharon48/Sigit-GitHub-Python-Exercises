46926d56e8e07ef91fee4918f035a1e9be8e2209
talsharon48 <talsharon48@gmail.com>
2019-03-06 17:43:36 +0200

    Sum Numbers In String Of Numbers Seporated By Commas

--- Sigit_EX2.py
+++ Sigit_EX2.py
@@ -1,3 +1,13 @@
+# function that gets a list of integers and sum them
+def sum_list_numbers():
+    sum_of_numbers = 0
+    list_of_numbers = input("Please Enter Numbers Separated By Commas: ").split(',')
+    for number in list_of_numbers:
+        sum_of_numbers += int(number)
+    print("The Sum Of The Numbers Is: ", sum_of_numbers)
+    # Also Can Be Done Like: print("The Sum Of The Numbers Is: " + sum(array_of_nums))
+
+
 # function that ask the user to enter numbers until "Stop"
 def sum_numbers():
     sum_of_numbers = 0
@@ -14,6 +24,7 @@ def sum_numbers():
 # main function that call the functions above
 def main():
     sum_numbers()
+    sum_list_numbers()
 
 if __name__ == '__main__':
     main()
